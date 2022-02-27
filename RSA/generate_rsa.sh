fe07fe18e495f17adca4632df596c3bad2e1389f
Corentin Mattern <34362921+cmattern68@users.noreply.github.com>
2022-02-27 23:06:39 +0100

    update gitignore

--- RSA/generate_rsa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-ssh-keygen -t rsa -b 2048 -m PEM -f jwtRS256.key
-# Don't add passphrase
-openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
-cat jwtRS256.key
-cat jwtRS256.key.pub
